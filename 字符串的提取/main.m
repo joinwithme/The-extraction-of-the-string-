04ab938761e2be6d294c9ff2fcfe938282be1cc8
qianfeng001 <qianfeng001@xiabowen.local>
2015-06-11 20:18:04 +0800

    Initial Commit

---
+++ 字符串的提取/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  字符串的提取
+//
+//  Created by qianfeng001 on 15/6/11.
+//  Copyright (c) 2015年 伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
